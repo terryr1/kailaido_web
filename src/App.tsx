122847e14d328430aabd21d87a90a0df09a06035
Terry Roy <terryroy@Terrys-MacBook-Pro.local>
2026-02-09 11:25:58 -0500

    test auto deploy

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          kailaido count is {count}
+          LETS GOOO count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
