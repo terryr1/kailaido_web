008b19df91dbd5e29dfd8344bd2bf48afdc70c8b
Terry Roy <terryroy@Terrys-MacBook-Pro.local>
2026-02-09 16:49:33 -0500

    fix build failure

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { PlusIcon } from '@heroicons/react/24/solid'
 import TouchableOpacity from './components/TouchableOpacity'
